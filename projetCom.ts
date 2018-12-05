7784785494b9c4d3fd336891cb817810aafd72e2
Clarisse <lou.claris@gmail.com>
2018-12-05 15:13:08 +0100

    ts caca

---
+++ projetCom.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic button-toggles
+ */
+@Component({
+  selector: 'buttons',
+  templateUrl: 'projetCom.html',
+  styleUrls: ['projetCom.css'],
+})
+export class ButtonToggleOverviewExample {}
